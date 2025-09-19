170eb8dcdfa000a72ea38ebab53a233532e51dfe
ehdgnsl01 <gnsl505@naver.com>
2025-09-19 16:45:32 +0900

    categories 수정

--- src/models/categories.ts
+++ src/models/categories.ts
@@ -1,7 +1,7 @@
 export const CATEGORIES = [
   { value: "category1", label: "Commercial" },
-  { value: "category2", label: "Youtube" },
-  { value: "category3", label: "Motion Graphic" },
+  { value: "category3", label: "Youtube" },
+  { value: "category2", label: "Motion Graphic" },
 ] as const;
 
 export type CategoryValue = (typeof CATEGORIES)[number]["value"];
